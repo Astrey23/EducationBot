de432f32a9dcf704f3f1c4004873ccdfb3caf9fb
Astrey <antonovanton133@gmail.com>
2024-10-08 17:40:15 +0300

    чевапчич

---
+++ ConsoleApp1/Commands/AdCommand.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp1.Entities;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using User = ConsoleApp1.Entities.User;
+
+namespace ConsoleApp1.Commands;
+
+public class AdCommand : ICommand
+{
+    private readonly List<User> _users;
+
+    public AdCommand(List<User> users)
+    {
+        _users = users;
+    }
+
+    public async Task ExecuteAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+    {
+        var senderId = update.Message.From?.Id!;
+        const string message = "Жду сообщение для рассылки";
+        await client.SendTextMessageAsync(senderId, message, cancellationToken: cancellationToken);
+        var user = _users.First(u => u.Id == update.Message.From.Id);
+        user.State = UserState.WaitingForSending;
+
+    } 
+    
+
+    public bool CanBeExecuted(Update update)
+    {
+        if(update.Message is not { Type: MessageType.Text, Text: "/ad" }) return false;
+        var user = _users.FirstOrDefault(u => u.Id == update.Message.From.Id);
+        return user?.State == UserState.Main;
+    }
+}
---
+++ ConsoleApp1/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1.Entities;
+
+public class User
+{
+    public required long Id { get ; init; }
+    public UserState State { get ; set ; }
+    
+    
+}
